783186e1bcdb5f1f8f195a0032fd2f514801a026
Prateek Ralhan <ralhanprateek@gmail.com>
2020-08-02 16:13:35 +0530

    Add files via upload

--- python_youtube_downloader.py
+++ python_youtube_downloader.py
@@ -7,15 +7,14 @@ from tkinter import ttk
 NORM_FONT= ("Verdana", 10)
 
 def entry_fields():
-    download_location = "youtube_downloads/"
-    Youtube(e1.get()).streams.first().download(download_location)
     start_time=time.time()
-    print(e1.get())
+    download_location = "youtube_downloads/"
+    YouTube(e1.get()).streams.first().download(download_location)
+    end_time=time.time()
     popup = tk.Tk()
     popup.wm_title("Download Status")
     str1="download successful !!\n"
-    end_time=time.time()
-    str2 = "Total time taken: {} seconds".format(round(start_time-end_time,4))
+    str2 = "Total time taken: {} seconds".format(round(end_time-start_time,3))
     msg=str1+str2
     label = ttk.Label(popup, text=msg, font=NORM_FONT)
     label.pack(side="top", fill="x", pady=10)
@@ -26,7 +25,7 @@ def entry_fields():
 
 master=tk.Tk()
 master.geometry("345x60")
-master.wm_title("Python based Youtube Downloader")
+master.wm_title("{Py}Youtube Downloader")
 tk.Label(master,text="Enter Youtube Video URL: ").grid(row=0)
 e1 = tk.Entry(master)
 e1.grid(row=0,column=1)
@@ -34,7 +33,7 @@ e1.grid(row=0,column=1)
 tk.Button(master,
           text='Download', command=entry_fields,anchor=tk.CENTER).grid(row=1,
                                                        sticky=tk.W,
-                                                       pady=4)
+                                                       pady=4)          
 tk.mainloop()
 
 #video_url = input("Please enter the Youtube video link/URL: ")
